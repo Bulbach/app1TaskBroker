d605ea4d4ef93e7b05c06a5c52c6d05f3b88a1df
Bulbash <zverovik@yahoo.com>
2023-01-15 04:36:29 +0300

    deleted configuration for ActiveMQ and remarks

--- src/main/java/com/alexIntervale1/app1/App1Application.java
+++ src/main/java/com/alexIntervale1/app1/App1Application.java
@@ -3,9 +3,11 @@ package com.alexIntervale1.app1;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.jms.annotation.EnableJms;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
 @EnableJms
+@EnableAsync
 public class App1Application {
 
 	public static void main(String[] args) {
--- src/main/java/com/alexIntervale1/app1/broker/BaseMessage.java
+++
@@ -1,8 +0,0 @@
-package com.alexIntervale1.app1.broker;
-
-public class BaseMessage {
-    private String uuid;
-    private String message;
-    private String code;
-
-}
