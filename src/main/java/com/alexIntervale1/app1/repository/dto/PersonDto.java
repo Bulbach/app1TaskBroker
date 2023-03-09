ef13d5a38028e109f17a3bd6dd9fc19b0b0d2432
Bulbash <zverovik@yahoo.com>
2023-03-09 15:16:55 +0300

    added annotation for return to XML format

--- src/main/java/com/alexIntervale1/app1/repository/dto/PersonDto.java
+++ src/main/java/com/alexIntervale1/app1/repository/dto/PersonDto.java
@@ -4,18 +4,27 @@ import lombok.Data;
 
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlElement;
+import javax.xml.bind.annotation.XmlRootElement;
 import java.io.Serializable;
 
+@XmlRootElement(
+        namespace = "http://intervale.ru/spring"
+        , name = "person"
+)
 @Data
 public class PersonDto implements Serializable {
     private static final long serialVersionUID = 1;
     @NotBlank
     @Size(max = 20)
+    @XmlElement
     private String name;
     @NotBlank
     @Size(max = 30)
+    @XmlElement
     private String surname;
     @NotBlank
-    @Size(min=12, max=12)
+    @Size(min = 12, max = 12)
+    @XmlElement
     private String personalNumber;
 }
